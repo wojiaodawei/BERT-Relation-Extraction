0ed219b532964402e201b6c175cc77c8cbb2fb15
Manuel Schmitz <manuel.schmitz@gmx.net>
2020-07-23 18:58:20 +0200

    Report Test BCE

--- train/pretrain.py
+++ train/pretrain.py
@@ -1,17 +1,19 @@
 import logging
 import os
 import time
+from itertools import combinations
 
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from ml_utils.common import valncreate_dir
-from torch import optim
+from torch import nn, optim
 from torch.nn.utils import clip_grad_norm_
 
 from constants import LOG_DATETIME_FORMAT, LOG_FORMAT, LOG_LEVEL
 from dataloaders.mtb_data_loader import MTBPretrainDataLoader
-from src.model.ALBERT.modeling_albert import AlbertModel
+from model.albert.albert import AlbertModel
+from model.bert.bert import BertModel
 from src.train_funcs import Two_Headed_Loss
 
 logging.basicConfig(
@@ -35,11 +37,18 @@ class Pretrainer:
         self.train_len = len(self.data_loader.train_generator)
         logger.info("Loaded %d pre-training samples." % self.train_len)
 
-        self.model = AlbertModel.from_pretrained(
-            model_size=self.transformer,
-            force_download=False,
-            pretrained_model_name_or_path=self.transformer,
-        )
+        if "albert" in self.transformer:
+            self.model = AlbertModel.from_pretrained(
+                model_size=self.transformer,
+                force_download=False,
+                pretrained_model_name_or_path=self.transformer,
+            )
+        else:
+            self.model = BertModel.from_pretrained(
+                model_size=self.transformer,
+                pretrained_model_name_or_path=self.transformer,
+                force_download=False,
+            )
 
         self.tokenizer = self.data_loader.tokenizer
         self.model.resize_token_embeddings(len(self.tokenizer))
@@ -55,7 +64,7 @@ class Pretrainer:
         self.criterion = Two_Headed_Loss(
             lm_ignore_idx=self.tokenizer.pad_token_id,
             use_logits=True,
-            normalize=False,
+            normalize=True,
         )
         self.optimizer = optim.Adam(
             [{"params": self.model.parameters(), "lr": self.config.get("lr")}]
@@ -68,11 +77,11 @@ class Pretrainer:
         )
 
         self._start_epoch = 0
-        self._best_mse = 0
-        self._global_loss = []
+        self._best_mtb_bce = 1
+        self._train_loss = []
         self._train_lm_acc = []
         self._lm_acc = []
-        self._blanks_mse = []
+        self._mtb_bce = []
 
     def load_best_model(self, checkpoint_dir: str):
         """
@@ -106,8 +115,8 @@ class Pretrainer:
         if resume and os.path.exists(best_model_path):
             (
                 self._start_epoch,
-                self._best_mse,
-                self._global_loss,
+                self._best_mtb_bce,
+                self._train_loss,
                 self._train_lm_acc,
             ) = self.load_best_model(checkpoint_dir)
 
@@ -117,19 +126,27 @@ class Pretrainer:
         for epoch in range(self._start_epoch, self.config.get("epochs")):
             start_time = time.time()
             self.model.train()
-            total_loss = 0.0
-            losses_per_batch = []
-            total_acc = 0.0
-            lm_accuracy_per_batch = []
+            train_loss = 0.0
+            train_loss_per_batch = []
+            train_acc = 0.0
+            train_lm_acc_per_batch = []
+            train_mtb_bce = 0.0
+            train_mtb_bce_per_batch = []
             for i, data in enumerate(self.data_loader.train_generator):
-                (x, masked_label, e1_e2_start, blank_labels) = data
+                sequence, masked_label, e1_e2_start, blank_labels = data
                 masked_label = masked_label[(masked_label != pad_id)]
                 if masked_label.shape[0] == 0:
-                    logger.warning("Empty dataset. Skipping")
                     continue
-                blanks_logits, lm_logits, masked_label = self._get_logits(
-                    e1_e2_start, masked_label, x
+                if self.train_on_gpu:
+                    masked_label = masked_label.cuda()
+                blanks_logits, lm_logits = self._get_logits(
+                    e1_e2_start, sequence
                 )
+
+                if lm_logits.size(0) != masked_label.size(0):
+                    blanks_logits, lm_logits = self._get_logits(
+                        e1_e2_start, sequence
+                    )
 
                 loss = self.criterion(
                     lm_logits, blanks_logits, masked_label, blank_labels,
@@ -146,67 +163,71 @@ class Pretrainer:
                     self.optimizer.step()
                     self.optimizer.zero_grad()
 
-                total_loss += loss.item()
-                total_acc += self.calculate_metrics(
+                train_loss += loss.item()
+                train_metrics = self.calculate_metrics(
                     lm_logits, blanks_logits, masked_label, blank_labels,
-                )[0]
-
+                )
+                train_acc += train_metrics[0]
+                train_mtb_bce += train_metrics[1]
                 if (i % update_size) == (update_size - 1):
-                    losses_per_batch.append(
-                        self.gradient_acc_steps * total_loss / update_size
+                    train_loss_per_batch.append(
+                        self.gradient_acc_steps * train_loss / update_size
                     )
-                    lm_accuracy_per_batch.append(total_acc / update_size)
+                    train_lm_acc_per_batch.append(train_acc / update_size)
+                    train_mtb_bce_per_batch.append(train_mtb_bce / update_size)
                     logger.info(
                         "Epoch {0} - {1}/{2} points]:".format(
                             epoch + 1, (i + 1), self.train_len
                         )
                     )
                     logger.info(
-                        "Total loss: {0}, LM accuracy per batch: {1}".format(
-                            losses_per_batch[-1], lm_accuracy_per_batch[-1],
+                        "Train loss: {0}, Train LM accuracy: {1}, Train MTB Binary Cross Entropy {2}".format(
+                            train_loss_per_batch[-1],
+                            train_lm_acc_per_batch[-1],
+                            train_mtb_bce_per_batch[-1],
                         )
                     )
-                    total_loss = 0.0
-                    total_acc = 0.0
+                    train_loss = 0.0
+                    train_acc = 0.0
+                    train_mtb_bce = 0.0
 
             eval_result = self.evaluate()
             self._lm_acc += [eval_result[0]]
-            self._blanks_mse += [eval_result[1]]
+            self._mtb_bce += [eval_result[1]]
 
             self.scheduler.step()
-            self._global_loss.append(np.mean(losses_per_batch))
-            self._train_lm_acc.append(np.mean(lm_accuracy_per_batch))
+            self._train_loss.append(np.mean(train_loss_per_batch))
+            self._train_lm_acc.append(np.mean(train_lm_acc_per_batch))
             logger.info(
                 "Epoch finished, took {0} seconds.".format(
                     time.time() - start_time
                 )
             )
-            logger.info("Losses at Epoch {0}".format(epoch + 1))
-            logger.info("Global Loss: {0}".format(self._global_loss[-1]))
+            logger.info("Loss at Epoch {0}".format(epoch + 1))
+            logger.info("Loss: {0}".format(self._train_loss[-1]))
             logger.info(
-                "Train Accuracy Loss: {0}".format(self._train_lm_acc[-1])
+                "Train LM Accuracy: {0}".format(self._train_lm_acc[-1])
             )
             logger.info("Validation LM Accuracy: {0}".format(self._lm_acc[-1]))
             logger.info(
-                "Validation Blanks MSE Loss: {0}".format(self._blanks_mse[-1])
+                "Validation MTB Binary Cross Entropy: {0}".format(
+                    self._mtb_bce[-1]
+                )
             )
 
-            if self._blanks_mse[-1] < self._best_mse:
-                self._best_mse = self._train_lm_acc[-1]
-                self._save_model(checkpoint_dir, epoch)
+            if self._mtb_bce[-1] < self._best_mtb_bce:
+                self._best_mtb_bce = self._mtb_bce[-1]
+                self._save_model(checkpoint_dir, epoch, best_model=True)
 
             self._save_model(
-                os.path.join(
-                    checkpoint_dir, "checkpoint_epoch_{0}.pth.tar"
-                ).format(epoch + 1),
-                epoch,
+                checkpoint_dir, epoch,
             )
 
         logger.info("Finished Training!")
         fig = plt.figure(figsize=(20, 20))
         ax = fig.add_subplot(111)
         ax.scatter(
-            np.arange(len(self._global_loss)), self._global_loss,
+            np.arange(len(self._train_loss)), self._train_loss,
         )
         ax.tick_params(axis="both", length=2, width=1, labelsize=14)
         ax.set_xlabel("Epoch", fontsize=22)
@@ -227,7 +248,13 @@ class Pretrainer:
 
         return self.model
 
-    def _save_model(self, path, epoch):
+    def _save_model(self, path, epoch, best_model: bool = False):
+        if best_model:
+            model_path = os.path.join(path, "best_model.pth.tar")
+        else:
+            model_path = os.path.join(
+                path, "checkpoint_epoch_{0}.pth.tar"
+            ).format(epoch + 1)
         torch.save(
             {
                 "epoch": epoch + 1,
@@ -235,31 +262,29 @@ class Pretrainer:
                 "best_acc": self._train_lm_acc[-1],
                 "optimizer": self.optimizer.state_dict(),
                 "scheduler": self.scheduler.state_dict(),
-                "losses_per_epoch": self._global_loss,
-                "accuracy_per_epoch": self._global_loss,
+                "losses_per_epoch": self._train_loss,
+                "accuracy_per_epoch": self._train_loss,
                 "lm_acc": self._lm_acc,
-                "blanks_mse": self._blanks_mse,
+                "blanks_mse": self._mtb_bce,
             },
-            os.path.join(path, "best_model.pth.tar"),
+            model_path,
         )
 
-    def _get_logits(self, e1_e2_start, masked_label, x):
+    def _get_logits(self, e1_e2_start, x):
         attention_mask = (x != self.tokenizer.pad_token_id).float()
         token_type_ids = torch.zeros((x.shape[0], x.shape[1])).long()
         if self.train_on_gpu:
             x = x.cuda()
-            masked_label = masked_label.cuda()
             attention_mask = attention_mask.cuda()
             token_type_ids = token_type_ids.cuda()
         blanks_logits, lm_logits = self.model(
             x,
             token_type_ids=token_type_ids,
             attention_mask=attention_mask,
-            Q=None,
             e1_e2_start=e1_e2_start,
         )
         lm_logits = lm_logits[(x == self.tokenizer.mask_token_id)]
-        return blanks_logits, lm_logits, masked_label
+        return blanks_logits, lm_logits
 
     def evaluate(self) -> tuple:
         """
@@ -276,28 +301,30 @@ class Pretrainer:
                 (x, masked_label, e1_e2_start, blank_labels) = data
                 masked_label = masked_label[(masked_label != pad_id)]
                 if masked_label.shape[0] == 0:
-                    logger.warning("Empty dataset. Skipping")
                     continue
-                blanks_logits, lm_logits, masked_label = self._get_logits(
-                    e1_e2_start, masked_label, x
-                )
+                if self.train_on_gpu:
+                    masked_label = masked_label.cuda()
+                blanks_logits, lm_logits = self._get_logits(e1_e2_start, x)
 
                 loss = self.criterion(
                     lm_logits, blanks_logits, masked_label, blank_labels,
                 )
 
                 total_loss += loss.cpu().numpy()
-                eval_result = Pretrainer.calculate_metrics(
-                    lm_logits, blanks_logits, masked_label, blank_labels,
+                eval_result = self.calculate_metrics(
+                    lm_logits, blanks_logits, masked_label, blank_labels
                 )
                 lm_acc += [eval_result[0]]
                 blanks_mse += [eval_result[1]]
         self.model.train()
-        return np.mean(lm_acc), np.mean(blanks_mse)
+        return (
+            np.mean(lm_acc),
+            sum(b for b in blanks_mse if b != 1)
+            / len([b for b in blanks_mse if b != 1]),
+        )
 
-    @classmethod
     def calculate_metrics(
-        cls, lm_logits, blanks_logits, masked_for_pred, blank_labels,
+        self, lm_logits, blanks_logits, masked_for_pred, blank_labels,
     ) -> tuple:
         """
         Calculates the performance metrics of the MTB model.
@@ -314,12 +341,50 @@ class Pretrainer:
             / len(masked_for_pred)
         ).item()
 
-        if blank_labels.is_cuda:
-            blanks_diff = (blanks_logits - blank_labels) ** 2
+        pos_idxs = [
+            i for i, l in enumerate(blank_labels.squeeze().tolist()) if l == 1
+        ]
+        neg_idxs = [
+            i for i, l in enumerate(blank_labels.squeeze().tolist()) if l == 0
+        ]
+
+        if len(pos_idxs) > 1:
+            # positives
+            pos_logits = []
+            for pos1, pos2 in combinations(pos_idxs, 2):
+                pos_logits.append(
+                    self._get_mtb_logits(
+                        blanks_logits[pos1, :], blanks_logits[pos2, :]
+                    )
+                )
+            pos_logits = torch.stack(pos_logits, dim=0)
+            pos_labels = [1.0 for _ in range(pos_logits.shape[0])]
         else:
-            blanks_diff = (blanks_logits - blank_labels.cuda()) ** 2
+            pos_logits, pos_labels = torch.FloatTensor([]), []
+            if blanks_logits.is_cuda:
+                pos_logits = pos_logits.cuda()
 
-        blanks_diff = blanks_diff.cpu().detach().numpy().sum()
-        blanks_mse = blanks_diff / len(blank_labels)
+        # negatives
+        neg_logits = []
+        for pos_idx in pos_idxs:
+            for neg_idx in neg_idxs:
+                neg_logits.append(
+                    Pretrainer._get_mtb_logits(
+                        blanks_logits[pos_idx, :], blanks_logits[neg_idx, :]
+                    )
+                )
+        neg_logits = torch.stack(neg_logits, dim=0)
+        neg_labels = [0.0 for _ in range(neg_logits.shape[0])]
 
-        return lm_accuracy, blanks_mse
+        blank_labels_ = torch.FloatTensor(pos_labels + neg_labels)
+        blank_pred = torch.cat([pos_logits, neg_logits], dim=0)
+        bce = nn.BCEWithLogitsLoss(reduction="mean")(
+            blank_pred.detach().cpu(), blank_labels_.detach().cpu()
+        )
+
+        return lm_accuracy, bce.numpy()
+
+    @classmethod
+    def _get_mtb_logits(cls, f1_vec, f2_vec):
+        factor = 1 / (torch.norm(f1_vec) * torch.norm(f2_vec))
+        return factor * torch.dot(f1_vec, f2_vec)
