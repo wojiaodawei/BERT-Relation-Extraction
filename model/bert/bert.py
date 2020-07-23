4957abfe07f6b80c44b0a31758efb49f3c01246d
Manuel Schmitz <manuel.schmitz@gmx.net>
2020-07-23 18:49:34 +0200

    3 Add bert

--- model/bert/bert.py
+++ model/bert/bert.py
@@ -1,22 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-PyTorch BERT model.
-"""
-
 from __future__ import (
     absolute_import,
     division,
@@ -29,11 +10,6 @@ import logging
 import torch
 from torch import nn
 from transformers import BertPreTrainedModel
-from transformers.file_utils import (
-    add_code_sample_docstrings,
-    add_start_docstrings,
-    add_start_docstrings_to_callable,
-)
 from transformers.modeling_bert import (
     BertEmbeddings,
     BertEncoder,
@@ -46,86 +22,33 @@ logger = logging.getLogger(__name__)
 _TOKENIZER_FOR_DOC = "BertTokenizer"
 
 
-BERT_START_DOCSTRING = r"""
-    This model is a PyTorch `torch.nn.Module <https://pytorch.org/docs/stable/nn.html#torch.nn.Module>`_ sub-class.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general
-    usage and behavior.
-
-    Parameters:
-        config (:class:`~transformers.BertConfig`): Model configuration class with all the parameters of the model.
-            Initializing with a config file does not load the weights associated with the model, only the configuration.
-            Check out the :meth:`~transformers.PreTrainedModel.from_pretrained` method to load the model weights.
-"""
-
-BERT_INPUTS_DOCSTRING = r"""
-    Args:
-        input_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`):
-            Indices of input sequence tokens in the vocabulary.
-
-            Indices can be obtained using :class:`transformers.BertTokenizer`.
-            See :func:`transformers.PreTrainedTokenizer.encode` and
-            :func:`transformers.PreTrainedTokenizer.__call__` for details.
-
-            `What are input IDs? <../glossary.html#input-ids>`__
-        attention_mask (:obj:`torch.FloatTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
-            Mask to avoid performing attention on padding token indices.
-            Mask values selected in ``[0, 1]``:
-            ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
-
-            `What are attention masks? <../glossary.html#attention-mask>`__
-        token_type_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
-            Segment token indices to indicate first and second portions of the inputs.
-            Indices are selected in ``[0, 1]``: ``0`` corresponds to a `sentence A` token, ``1``
-            corresponds to a `sentence B` token
-
-            `What are token type IDs? <../glossary.html#token-type-ids>`_
-        position_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
-            Indices of positions of each input sequence tokens in the position embeddings.
-            Selected in the range ``[0, config.max_position_embeddings - 1]``.
-
-            `What are position IDs? <../glossary.html#position-ids>`_
-        head_mask (:obj:`torch.FloatTensor` of shape :obj:`(num_heads,)` or :obj:`(num_layers, num_heads)`, `optional`, defaults to :obj:`None`):
-            Mask to nullify selected heads of the self-attention modules.
-            Mask values selected in ``[0, 1]``:
-            :obj:`1` indicates the head is **not masked**, :obj:`0` indicates the head is **masked**.
-        inputs_embeds (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`, defaults to :obj:`None`):
-            Optionally, instead of passing :obj:`input_ids` you can choose to directly pass an embedded representation.
-            This is useful if you want more control over how to convert `input_ids` indices into associated vectors
-            than the model's internal embedding lookup matrix.
-        encoder_hidden_states  (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`, defaults to :obj:`None`):
-            Sequence of hidden-states at the output of the last layer of the encoder. Used in the cross-attention
-            if the model is configured as a decoder.
-        encoder_attention_mask (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
-            Mask to avoid performing attention on the padding token indices of the encoder input. This mask
-            is used in the cross-attention if the model is configured as a decoder.
-            Mask values selected in ``[0, 1]``:
-            ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
-        output_attentions (:obj:`bool`, `optional`, defaults to :obj:`None`):
-            If set to ``True``, the attentions tensors of all attention layers are returned. See ``attentions`` under returned tensors for more detail.
-"""
-
-
-@add_start_docstrings(
-    "The bare Bert Model transformer outputting raw hidden-states without any specific head on top.",
-    BERT_START_DOCSTRING,
-)
 class BertModel(BertPreTrainedModel):
-    """
-    The model can behave as an encoder (with only self-attention) as well as a
-    decoder, in which case a layer of cross-attention is added between the
-    self-attention layers, following the architecture described in `Attention
-    is all you need`_ by Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob
-    Uszkoreit, Llion Jones, Aidan N. Gomez, Lukasz Kaiser and Illia Polosukhin.
+    def __init__(self, config, model_size, task=None, n_classes=None):
+        """
+        The bare Bert Model transformer outputting raw hidden-states without
+        any specific head on top.
 
-    To behave as an decoder the model needs to be initialized with the
-    :obj:`is_decoder` argument of the configuration set to :obj:`True`; an
-    :obj:`encoder_hidden_states` is expected as an input to the forward pass.
+        The model can behave as an encoder (with only self-attention) as well as a
+        decoder, in which case a layer of cross-attention is added between the
+        self-attention layers, following the architecture described in `Attention
+        is all you need`_ by Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob
+        Uszkoreit, Llion Jones, Aidan N. Gomez, Lukasz Kaiser and Illia Polosukhin.
 
-    .. _`Attention is all you need`:
-        https://arxiv.org/abs/1706.03762
-    """
+        This model is a PyTorch `torch.nn.Module <https://pytorch.org/docs/stable/nn.html#torch.nn.Module>`_ sub-class.
+        Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general
+        usage and behavior.
 
-    def __init__(self, config, model_size, task=None, n_classes_=None):
+        Args:
+            config (:class:`~transformers.BertConfig`): Model configuration class with all the parameters of the model.
+                Initializing with a config file does not load the weights associated with the model, only the configuration.
+                Check out the :meth:`~transformers.PreTrainedModel.from_pretrained` method to load the model weights.
+            model_size: Size of the Model
+            task: MTB task
+            n_classes: Number of classes
+
+        References:
+            `Attention is all you need`: https://arxiv.org/abs/1706.03762
+        """
         super(BertModel, self).__init__(config)
         self.config = config
 
@@ -139,43 +62,34 @@ class BertModel(BertPreTrainedModel):
 
         print("Model config: ", self.config)
         if self.task is None:
-            ### blanks head ###
-            # self.blanks_linear = nn.Linear(1536, 1)
-            # self.sigmoid = nn.Sigmoid()
+            # blanks head
             self.activation = nn.Tanh()
-            ### LM head ###
-            self.cls = BertOnlyMLMHead(config)
-            # self.lm_linear = nn.Linear(768, self.config.vocab_size)
-            # self.lm_bias = nn.Parameter(torch.zeros(config.vocab_size))
+            # LM head
+            self.lm_classificattion = BertOnlyMLMHead(config)
         elif self.task == "classification":
-            self.n_classes_ = n_classes_
+            self.n_classes_ = n_classes
             if self.model_size == "bert-base-uncased":
-                self.classification_layer = nn.Linear(1536, n_classes_)
+                self.classification_layer = nn.Linear(1536, n_classes)
             elif self.model_size == "bert-large-uncased":
-                self.classification_layer = nn.Linear(2048, n_classes_)
+                self.classification_layer = nn.Linear(2048, n_classes)
 
-    def get_input_embeddings(self):
+    def get_input_embeddings(self):  # noqa: D102
         return self.embeddings.word_embeddings
 
-    def set_input_embeddings(self, value):
+    def set_input_embeddings(self, value):  # noqa: D102
         self.embeddings.word_embeddings = value
 
     def _prune_heads(self, heads_to_prune):
         """
         Prunes heads of the model.
 
-        heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
-        See base class PreTrainedModel
+        Args:
+            heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
+            See base class PreTrainedModel
         """
         for layer, heads in heads_to_prune.items():
             self.encoder.layer[layer].attention.prune_heads(heads)
 
-    @add_start_docstrings_to_callable(
-        BERT_INPUTS_DOCSTRING.format("(batch_size, sequence_length)")
-    )
-    @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC, checkpoint="bert-base-uncased"
-    )
     def forward(
         self,
         input_ids=None,
@@ -190,8 +104,50 @@ class BertModel(BertPreTrainedModel):
         output_hidden_states=None,
         e1_e2_start=None,
     ):
-        r"""
-        Return:
+        """
+        Forward pass of BERT.
+
+        Args:
+            input_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`):
+                Indices of input sequence tokens in the vocabulary.
+                Indices can be obtained using :class:`transformers.BertTokenizer`.
+                See :func:`transformers.PreTrainedTokenizer.encode` and
+                :func:`transformers.PreTrainedTokenizer.__call__` for details.
+                `What are input IDs? <../glossary.html#input-ids>`__
+            attention_mask (:obj:`torch.FloatTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
+                Mask to avoid performing attention on padding token indices.
+                Mask values selected in ``[0, 1]``:
+                ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
+                `What are attention masks? <../glossary.html#attention-mask>`__
+            token_type_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
+                Segment token indices to indicate first and second portions of the inputs.
+                Indices are selected in ``[0, 1]``: ``0`` corresponds to a `sentence A` token, ``1``
+                corresponds to a `sentence B` token
+                `What are token type IDs? <../glossary.html#token-type-ids>`_
+            position_ids (:obj:`torch.LongTensor` of shape :obj:`{0}`, `optional`, defaults to :obj:`None`):
+                Indices of positions of each input sequence tokens in the position embeddings.
+                Selected in the range ``[0, config.max_position_embeddings - 1]``.
+                `What are position IDs? <../glossary.html#position-ids>`_
+            head_mask (:obj:`torch.FloatTensor` of shape :obj:`(num_heads,)` or :obj:`(num_layers, num_heads)`, `optional`, defaults to :obj:`None`):
+                Mask to nullify selected heads of the self-attention modules.
+                Mask values selected in ``[0, 1]``:
+                :obj:`1` indicates the head is **not masked**, :obj:`0` indicates the head is **masked**.
+            inputs_embeds (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`, defaults to :obj:`None`):
+                Optionally, instead of passing :obj:`input_ids` you can choose to directly pass an embedded representation.
+                This is useful if you want more control over how to convert `input_ids` indices into associated vectors
+                than the model's internal embedding lookup matrix.
+            encoder_hidden_states  (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`, defaults to :obj:`None`):
+                Sequence of hidden-states at the output of the last layer of the encoder. Used in the cross-attention
+                if the model is configured as a decoder.
+            encoder_attention_mask (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
+                Mask to avoid performing attention on the padding token indices of the encoder input. This mask
+                is used in the cross-attention if the model is configured as a decoder.
+                Mask values selected in ``[0, 1]``:
+                ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
+            output_attentions (:obj:`bool`, `optional`, defaults to :obj:`None`):
+                If set to ``True``, the attentions tensors of all attention layers are returned. See ``attentions`` under returned tensors for more detail.
+
+        Returns:
             :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.BertConfig`) and inputs:
             last_hidden_state (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`):
                 Sequence of hidden-states at the output of the last layer of the model.
@@ -215,7 +171,7 @@ class BertModel(BertPreTrainedModel):
 
                 Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
                 heads.
-            """
+        """
         output_attentions = (
             output_attentions
             if output_attentions is not None
@@ -306,20 +262,18 @@ class BertModel(BertPreTrainedModel):
         sequence_output = encoder_outputs[0]
         self.pooler(sequence_output)
 
-        ### two heads: LM and blanks ###
+        ### two heads: LM and blanks
         blankv1v2 = sequence_output[:, e1_e2_start, :]
         buffer = []
         for i in range(blankv1v2.shape[0]):  # iterate batch & collect
             v1v2 = blankv1v2[i, i, :, :]
             v1v2 = torch.cat((v1v2[0], v1v2[1]))
             buffer.append(v1v2)
-        del blankv1v2
         v1v2 = torch.stack([a for a in buffer], dim=0)
-        del buffer
 
         if self.task is None:
             blanks_logits = self.activation(v1v2)
-            lm_logits = self.cls(sequence_output)
+            lm_logits = self.lm_classificattion(sequence_output)
             return blanks_logits, lm_logits
         elif self.task == "classification":
             classification_logits = self.classification_layer(v1v2)
