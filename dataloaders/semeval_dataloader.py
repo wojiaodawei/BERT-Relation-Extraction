f60dbd02d81a765aa0cb11508166f481a85a9fc0
Manuel Schmitz <manuel.schmitz@gmx.net>
2020-10-01 21:19:21 +0200

    10 Refactor SemEval Dataloader

--- dataloaders/semeval_dataloader.py
+++ dataloaders/semeval_dataloader.py
@@ -35,7 +35,7 @@ class SemEvalDataloader:
         self.relations_mapper_path = "data/sem_eval/relations.pkl"
         self.trainset_path = "data/sem_eval/train.pkl"
         self.testset_path = "data/sem_eval/test.pkl"
-        df_train, df_test = self.load_dataset()
+        df_train, df_test = self._load_dataset()
         self.train_generator = SemEvalGenerator(
             data=df_train,
             tokenizer=self.tokenizer,
@@ -81,66 +81,56 @@ class SemEvalDataloader:
         """
         Data preprocessing for SemEval2010 task 8 dataset.
         """
-        train_data_path = self.config.get("train_file")
-        logger.info("Reading training file from {0}".format(train_data_path))
-        with open(train_data_path, "r", encoding="utf8") as train_file:
-            text = train_file.readlines()
-        sents, relations = self._preprocess_string(text)
-        e1_e2_start = []
-        logger.info("Tokenizing train data")
-        for trainset_idx, trainset_s in enumerate(tqdm(sents)):
-            trainset_s = self.tokenize_dataset(e1_e2_start, trainset_s)
-            sents[trainset_idx] = trainset_s
-        df_train = pd.DataFrame(
-            data={
-                "sequence": sents,
-                "relation": relations,
-                "e1_e2_start": e1_e2_start,
-            }
-        )
+        logger.info("Preprocessing train data")
+        df_train = self._preprocess_dataset(path=self.config.get("train_file"))
+        logger.info("Preprocessing test data")
+        df_test = self._preprocess_dataset(path=self.config.get("test_file"))
 
-        test_data_path = self.config.get("test_file")
-        logger.info("Reading test file from {0}".format(test_data_path))
-        with open(test_data_path, "r", encoding="utf8") as test_file:
-            text = test_file.readlines()
-        sents, relations = self._preprocess_string(text)
-        e1_e2_start = []
-        logger.info("Tokenizing test data")
-        for testset_idx, testset_s in enumerate(tqdm(sents)):
-            testset_s = self.tokenize_dataset(e1_e2_start, testset_s)
-            sents[testset_idx] = testset_s
-        df_test = pd.DataFrame(
-            data={
-                "sequence": sents,
-                "relation": relations,
-                "e1_e2_start": e1_e2_start,
-            }
-        )
+        rel2idx = {}
+        idx2rel = {}
 
-        rm = RelationsMap(df_train["relation"])
-        with open(self.relations_mapper_path, "wb") as rm_output:
-            joblib.dump(rm, rm_output)
-        logger.info(f"Saved relations map at {self.relations_mapper_path}")
+        logger.info("Mapping relations to IDs...")
+        for idx, relation in enumerate(df_train["relation"].unique()):
+            rel2idx[relation] = idx
+            idx2rel[idx] = relation
 
-        df_train["relation_id"] = df_train.progress_apply(
-            lambda x: rm.rel2idx[x["relation"]], axis=1
+        df_train["relation_id"] = df_train.apply(
+            lambda x: rel2idx[x["relation"]], axis=1
         )
         df_train.drop(columns=["relation"], inplace=True)
         with open(self.trainset_path, "wb") as train_output:
             joblib.dump(df_train, train_output)
         logger.info("Saved trainset at {0}".format(self.trainset_path))
 
-        df_test["relation_id"] = df_test.progress_apply(
-            lambda x: rm.rel2idx[x["relation"]], axis=1
+        df_test["relation_id"] = df_test.apply(
+            lambda x: rel2idx[x["relation"]], axis=1
         )
         df_test.drop(columns=["relation"], inplace=True)
         with open(self.testset_path, "wb") as test_output:
             joblib.dump(df_test, test_output)
         logger.info("Saved testset at {0}".format(self.testset_path))
 
-        return df_train, df_test, rm
+        return df_train, df_test
 
-    def tokenize_dataset(self, e1_e2_start, s):
+    def _preprocess_dataset(self, path: str):
+        logger.info("Reading file from {0}".format(path))
+        with open(path, "r", encoding="utf8") as file:
+            text = file.readlines()
+        sents, relations = self._preprocess_string(text)
+        e1_e2_start = []
+        logger.info("Tokenizing data")
+        for trainset_idx, trainset_s in enumerate(tqdm(sents)):
+            trainset_s = self._tokenize_x(e1_e2_start, trainset_s)
+            sents[trainset_idx] = trainset_s
+        return pd.DataFrame(
+            data={
+                "sequence": sents,
+                "relation": relations,
+                "e1_e2_start": e1_e2_start,
+            }
+        )
+
+    def _tokenize_x(self, e1_e2_start, s):
         s = (
             [self.tokenizer.cls_token]
             + self.tokenizer.tokenize(s)
@@ -171,31 +161,15 @@ class SemEvalDataloader:
             relations.append(relation)
         return sents, relations
 
-    def load_dataset(self):
-        if os.path.isfile(self.trainset_path) and os.path.isfile(
-            self.testset_path
-        ):
+    def _load_dataset(self):
+        trainset_exists = os.path.isfile(self.trainset_path)
+        testset_exists = os.path.isfile(self.testset_path)
+        if trainset_exists and testset_exists:
             df_train = joblib.load(self.trainset_path)
             df_test = joblib.load(self.testset_path)
             logger.info("Loaded preproccessed data.")
         else:
-            df_train, df_test, rm = self.preprocess()
+            df_train, df_test = self.preprocess()
 
         self.n_classes = len(df_train["relation_id"].unique())
         return df_train, df_test
-
-
-class RelationsMap(object):
-    def __init__(self, relations):
-        self.rel2idx = {}
-        self.idx2rel = {}
-
-        logger.info("Mapping relations to IDs...")
-        self.n_classes = 0
-        for relation in tqdm(relations):
-            if relation not in self.rel2idx.keys():
-                self.rel2idx[relation] = self.n_classes
-                self.n_classes += 1
-
-        for key, value in self.rel2idx.items():
-            self.idx2rel[value] = key
